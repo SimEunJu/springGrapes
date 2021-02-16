207c972c8ab8b938bc8b34ac38199251d19c699b
SimEunJu <wndmstla123@gmail.com>
2021-02-17 01:27:12 +0900

    fix: api 매칭 되도록

--- src/main/java/com/sej/grapes/controller/BunchGrapesController.java
+++ src/main/java/com/sej/grapes/controller/BunchGrapesController.java
@@ -12,7 +12,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("/api/grapes")
+@RestController
+@RequestMapping("/api/grapes")
 @AllArgsConstructor
 @Slf4j
 public class BunchGrapesController {
@@ -26,31 +27,31 @@ public class BunchGrapesController {
         return ResponseEntity.ok(bunchGrapesId);
     }
 
-    @GetMapping("/:bunchGrapesId")
+    @GetMapping("/{bunchGrapesId}")
     public ResponseEntity<BunchGrapesDto> getBunchGrapes(@PathVariable Long bunchGrapesId){
         BunchGrapesDto bunchGrapesDto = bunchGrapesService.getBunchGrapesById(bunchGrapesId);
         return ResponseEntity.ok(bunchGrapesDto);
     }
 
-    @DeleteMapping("/:bunchGrapesId")
+    @DeleteMapping("/{bunchGrapesId}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteBunchGrapes(@PathVariable Long bunchGrapesId){
         bunchGrapesService.delete(bunchGrapesId);
     }
 
-    @PostMapping("/:bunchGrapesId/rgba")
+    @PostMapping("/{bunchGrapesId}/rgba")
     @ResponseStatus(HttpStatus.OK)
     public void changeRgba(@PathVariable Long bunchGrapesId, @RequestParam String rgba){
         bunchGrapesService.updateRgba(bunchGrapesId, rgba);
     }
 
-    @PostMapping("/:bunchGrapesId/title")
+    @PostMapping("/{bunchGrapesId}/title")
     @ResponseStatus(HttpStatus.OK)
     public void changeTitle(@PathVariable Long bunchGrapesId, @RequestParam String title){
         bunchGrapesService.updateTitle(bunchGrapesId, title);
     }
 
-    @PostMapping("/:bunchGrapesId/finish")
+    @PostMapping("/{bunchGrapesId}/finish")
     @ResponseStatus(HttpStatus.OK)
     public void changeFinishStatus(@PathVariable Long bunchGrapesId){
         bunchGrapesService.updateFinishState(bunchGrapesId);
--- src/main/java/com/sej/grapes/controller/GrapeController.java
+++ src/main/java/com/sej/grapes/controller/GrapeController.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("/api/grapes/:bunchGrapesId/:grapeId")
+@RestController
+@RequestMapping("/api/grapes/{bunchGrapesId}/{grapeId}")
 @Slf4j
 @AllArgsConstructor
 public class GrapeController {
